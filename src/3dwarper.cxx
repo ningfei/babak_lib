29e9e9ec11bfca46ec98af7e534e14c21a50dcd2
Babak A. Ardekani, Ph.D <ardekani@nki.rfmh.org>
2016-05-09 14:29:16 -0400

    Version 2.0 of 3dwarper

--- src/3dwarper.cxx
+++ src/3dwarper.cxx
@@ -21,8 +21,16 @@
 #define YES 1
 #define NO 0
 #define MAXITER 10
+#define MAXR 15
+
+#define MATRIXSIZE 256
+#define VOXELSIZE 1.0
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
+
+// multi-resolution image dimensions
+DIM dim1, dim2, dim4, dim8;
+
 int opt;
 
 static struct option options[] =
@@ -32,7 +40,6 @@ static struct option options[] =
    {"-affine", 0, 'a'},
    {"-a", 0, 'a'},
    {"-I", 0, 'I'},
-   {"-HR", 0, 'H'},
    {"-hr", 0, 'H'},
 
    {"-iter8", 1, '8'},
@@ -40,8 +47,8 @@ static struct option options[] =
    {"-iter2", 1, '2'},
    {"-iter1", 1, '1'},
 
-   {"-trgorient",1,'R'},
-   {"-suborient",1,'S'},
+   {"-r", 1, 'r'},
+   {"-R", 1, 'R'},
 
    {"-T", 1, 'T'},
    {"-sub", 1, 's'},
@@ -65,14 +72,12 @@ int opt_cubicspline=NO;
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 extern float detect_lm(SPH &searchsph, SPH &testsph, SHORTIM testim, int lmcm[], SPH &refsph, int lm[]);
-DIM dim1, dim2, dim4, dim8;
 
 void print_help_and_exit()
 {
    printf("\n\nUsage:\n"
-   "\t3dwarper [-v or -verbose] [-h or -help] [-affine or -a] [-I] [-HR] [-version]\n"
-   "\t[-trgorient <orientation code>] [-suborient <orientation code>] [-u <filename>]\n"
-   "\t[-o <filename>] [-cubicspline]\n"
+   "\t3dwarper [-v or -verbose] [-h or -help] [-affine or -a] [-I] [-version]\n"
+   "\t[-u <filename>] [-o <filename>] [-cubicspline] [-r patch radius] [-R search radius]\n"
    "\t-sub <subject image> -trg <target image>\n\n" 
 
    "Required arguments:\n\n"
@@ -88,28 +93,23 @@ void print_help_and_exit()
    "\t-v or -verbose\n"
    "\t\tEnables verbose mode.\n\n"
 
+   "\t-r <patch radius>\n"
+   "\t\tSpecifies the patch radius (default=3 pixels).\n\n"
+
+   "\t-R <search radius>\n"
+   "\t\tSpecifies the serach radius (default=3 pixels).\n\n"
+
    "\t-h or -help\n"
    "\t\tPrints help message.\n\n"
 
    "\t-version\n"
    "\t\tPrints program vesion.\n\n"
 
-   "\t-HR\n"
-   "\t\tPut program in high-resolution mode (warning: will run forever!).\n\n"
-
    "\t-I\n"
    "\t\tDoes not perform initial linear registration of any kind.\n\n"
 
    "\t-affine or -a\n"
    "\t\tLimits the registration to an affine transformation.\n\n"
-
-   "\t-trgorient <orientation code>\n"
-   "\t\tOverrides the orientation information in the target image NIFTI header with the\n"
-   "\t\tgiven <orientation code> (PIL, LPS, etc.).\n\n"
-
-   "\t-suborient <orientation code>\n"
-   "\t\tOverrides the orientation information in the subject image NIFTI header with the\n"
-   "\t\tgiven <orientation code> (PIL, LPS, etc.).\n\n"
 
    "\t-u <filename>\n"
    "\t\tStores the displacement vector field in the specified <filename>\n"
@@ -475,72 +475,73 @@ void combine_warps_and_trans(float *trgTPIL, float *Xwarp, float *Ywarp, float *
 
 int main(int argc, char **argv)
 {
-   float I[16];
+   // input "subject image" full path
+   char subjectImageFile[1024]=""; 
+   // input "trget image" full path
+   char targetImageFile[1024]="";
 
-   char opt_affine=NO;
+   // extracted subject image filename only without suffix
+   char subprefix[1024]=""; 
+   // extracted target image filename only without suffix
+   char trgprefix[1024]="";
+
+   // original subject and target images loaded 
+   // from subjectImageFile and targetImageFile
+   short *sub, *trg;
+
+   // NIFTI headers for input subject and target images
+   nifti_1_header sub_hdr, trg_hdr;
+
+   // structures for holding subject and target image dimensions
+   DIM dim_sub, dim_trg;
+
+   // If this flag is set to YES, initial rigid-body + affine transformations are NOT performed
+   // subTPIL, trgTPIL, and sub_to_trg are all set to the identity matrix
    char opt_I=NO;
-   char opt_HR=NO;
 
-   int patch_r=3; // patch radius
-   int search_R=3; // search radius 
-   int MATRIXSIZE=256;
-   float VOXELSIZE=1.0; // mm units
+   // a linear transformation that brings the subject/target image from its native space to PIL space
+   float subTPIL[16], trgTPIL[16];
 
-   float *Xout, *Yout, *Zout; // final displacement field
-
-   char affineTransformationFile[1024]="";
-
-   short *subPIL1=NULL, *subPIL2=NULL, *subPIL4=NULL, *subPIL8=NULL;
-   short *trgPIL1=NULL, *trgPIL2=NULL, *trgPIL4=NULL, *trgPIL8=NULL;
-   short *mskPIL1=NULL, *mskPIL2=NULL, *mskPIL4=NULL, *mskPIL8=NULL;
-
-   FILE *fp;
-   char filename[1024];
-
-   char trgprefix[1024]=""; //target image prefix
-   char subprefix[1024]=""; //subject image prefix
-
-   getARTHOME();
-
-   DIM dim_trg;
-   DIM dim_sub;
-
-   // a linear transformation that brings the subject image from its native space to PIL space
-   float subTPIL[16]; 
-
-   // a linear transformation that brings the target image from its native space to PIL space
-   float trgTPIL[16];
-
-   char trglmfile[1024]="";
+   // filenames where AC, PC, and VSPS are manually specific for subject/target image
    char sublmfile[1024]="";
+   char trglmfile[1024]="";
 
+   // number if iterations of NL registration at each resolution level
    int iter8=4;
    int iter4=3;
    int iter2=2;
    int iter1=1;
 
-   nifti_1_header trg_hdr;
-   nifti_1_header sub_hdr;
+   // Subject and target PIL versions and brain mask at different resolutons
+   short *subPIL1=NULL, *subPIL2=NULL, *subPIL4=NULL, *subPIL8=NULL;
+   short *trgPIL1=NULL, *trgPIL2=NULL, *trgPIL4=NULL, *trgPIL8=NULL;
+   short *mskPIL1=NULL, *mskPIL2=NULL, *mskPIL4=NULL, *mskPIL8=NULL;
 
-   char subjectImageFile[1024]=""; 
-   char targetImageFile[1024]="";
-   char subOrient[4]="";  // orientation code for the subject image
-   char trgOrient[4]="";  // orientation code for the target image
-   char outputfile[1024]="";
-   char warpfile[1024]="";
+   // 4x4 identity matrix
+   float I[16];
 
+   // space holder for a generic filename
+   char filename[1024];
+
+   int patch_r=3; // patch radius
+   int search_R=3; // search radius 
+
+   // subject to target image affine transformation
    float sub_to_trg[16];
-   float *invT;		
-   float T[16];	// overall rigid body transformation
-   float *Xwarp=NULL, *Ywarp=NULL, *Zwarp=NULL;
 
-   short *sub;
-   int Snx,Sny,Snz;
-   float Sdx,Sdy,Sdz;
+   // if set to YES, only an affine registration is performed
+   char opt_affine=NO;
 
-   short *trg;
-   int Tnx,Tny,Tnz;
-   float Tdx,Tdy,Tdz;
+   // affine transformation file specified at input
+   char affineTransformationFile[1024]="";
+
+   // if specified at the command line, the non-linearly transformed subject 
+   // image will be saved under this name
+   char outputfile[1024]="";
+
+   // if specified at the command line, the final displacement field
+   // image will be saved under this name
+   char warpfile[1024]="";
 
    /////////////////////////////////////////////////////////////////////
    // maximize the stack size
@@ -564,11 +565,14 @@ int main(int argc, char **argv)
          case 'V':
             printf("Version 2.0 (last modified: May 17, 2016)\n");
             exit(0);
-         case 'H':
-            opt_HR= YES;
-            break;
          case 'I':
             opt_I= YES;
+            break;
+         case 'r':
+            patch_r = atoi(optarg);
+            break;
+         case 'R':
+            search_R = atoi(optarg);
             break;
          case 'a':
             opt_affine = YES;
@@ -588,17 +592,11 @@ int main(int argc, char **argv)
          case 'T':
             sprintf(affineTransformationFile,"%s",optarg);
             break;
-         case 'R':
-            sprintf(trgOrient,"%s",optarg);
-            break;
          case 'm':
             sprintf(trglmfile,"%s",optarg);
             break;
          case 'M':
             sprintf(sublmfile,"%s",optarg);
-            break;
-         case 'S':
-            sprintf(subOrient,"%s",optarg);
             break;
          case 'h':
             print_help_and_exit();
@@ -627,27 +625,22 @@ int main(int argc, char **argv)
 	}
 
    ////////////////////////////////////////////////////////////////////////////////////////////
+   // Variable initializations
+   getARTHOME();
 
-   if(opt_HR)
-   {
-      patch_r *= 2; 
-      search_R *= 2;
-      MATRIXSIZE *= 2;
-      VOXELSIZE /= 2.0; 
-   }
-
+   // Initialize dim1, dim2, dim4, and dim8 multi-resolution image dimensions
    dim1.nx = dim1.ny = dim1.nz = MATRIXSIZE;
-   dim1.np=dim1.nx*dim1.nx; 
+   dim1.np=dim1.nx*dim1.ny; 
    dim1.nv=dim1.np*dim1.nz; 
    dim1.dx = dim1.dy = dim1.dz = VOXELSIZE;
 
    dim2.nx = dim2.ny = dim2.nz = MATRIXSIZE/2;
-   dim2.np=dim2.nx*dim2.nx; 
+   dim2.np=dim2.nx*dim2.ny; 
    dim2.nv=dim2.np*dim2.nz; 
    dim2.dx = dim2.dy = dim2.dz = VOXELSIZE*2.0;
 
    dim4.nx = dim4.ny = dim4.nz = MATRIXSIZE/4;
-   dim4.np=dim4.nx*dim4.nx; 
+   dim4.np=dim4.nx*dim4.ny; 
    dim4.nv=dim4.np*dim4.nz; 
    dim4.dx = dim4.dy = dim4.dz = VOXELSIZE*4.0;
 
@@ -656,88 +649,90 @@ int main(int argc, char **argv)
    dim8.nv=dim8.np*dim8.nz; 
    dim8.dx = dim8.dy = dim8.dz = VOXELSIZE*8.0;
 
-   ////////////////////////////////////////////////////////////////////////////////////////////
-   
    // reset unreasonable values to default
    if(iter8 < 0 || iter8>MAXITER ) iter8=4;
    if(iter4 < 0 || iter4>MAXITER ) iter4=3;
    if(iter2 < 0 || iter2>MAXITER ) iter2=2;
    if(iter1 < 0 || iter1>MAXITER ) iter1=1;
 
+   if(patch_r <= 0 || patch_r > MAXR ) patch_r=3;
+   if(search_R <= 0 || search_R > MAXR ) search_R=3;
+
    ////////////////////////////////////////////////////////////////////////////////////////////
-
-   // ensure that subject and target images were specified at the command line
-
+   ////////////////////////////////////////////////////////////////////////////////////////////
+   // ensure that subject and target images are specified at the command line
    if( subjectImageFile[0] == '\0')
    {
-      printf("\nError: you must specify a \"subject image\" using the -sub argument.\n\n");		
+      printf("\nPlease specify a \"subject image\" using -sub <filename.nii> ...\n\n");		
       exit(0);
    } 
 
-   if( niftiFilename(subprefix, subjectImageFile)==0 ) { exit(0); }
-
    if( targetImageFile[0] == '\0')
    {
-      printf("\nError: you must specify a \"target image\" using the -trg argument.\n\n");		
+      printf("\nPlease specify a \"target image\" -trg <filename.nii> ...\n\n");		
       exit(0);
    }
+   ////////////////////////////////////////////////////////////////////////////////////////////
 
+   // extract subject and target filenames without path/suffix
+   if( niftiFilename(subprefix, subjectImageFile)==0 ) { exit(0); }
    if( niftiFilename(trgprefix, targetImageFile)==0 ) { exit(0); }
 
-   if(opt_v)
-   {
-      printf("Subject image file = %s\n",subjectImageFile);
-      printf("Target image file = %s\n",targetImageFile);
-   }
-
+   ////////////////////////////////////////////////////////////////////////////////////////////
+   // read subject and target images
    sub = (int2 *)read_nifti_image(subjectImageFile, &sub_hdr);
-   set_dim(dim_sub,sub_hdr);
-   Snx = dim_sub.nx; Sny = dim_sub.ny; Snz = dim_sub.nz;
-   Sdx = dim_sub.dx; Sdy = dim_sub.dy; Sdz = dim_sub.dz;
-
    if(sub==NULL) 
    {
       printf("\nError: Reading subject image %s failed.\n\n",subjectImageFile);
       exit(0);
    }
+   set_dim(dim_sub,sub_hdr); // transfer info from sub_hdr to dim_sub
 
    trg = (int2 *)read_nifti_image(targetImageFile, &trg_hdr);
-   set_dim(dim_trg,trg_hdr);
-   Tnx = dim_trg.nx; Tny = dim_trg.ny; Tnz = dim_trg.nz;
-   Tdx = dim_trg.dx; Tdy = dim_trg.dy; Tdz = dim_trg.dz;
-
    if(trg==NULL) 
    {
       printf("\nError: Reading target image %s failed.\n\n",targetImageFile);
       exit(0);
    }
+   set_dim(dim_trg,trg_hdr); // transfer info from trg_hdr to dim_trg
    ////////////////////////////////////////////////////////////////////////////////////////////
 
+   // print some info
+   if(opt_v)
+   {
+      printf("Subject image file: %s\n",subjectImageFile);
+      printf("\tMatrix size: %d x %d x %d\n",dim_sub.nx, dim_sub.ny, dim_sub.nz);
+      printf("\tVoxel size: %6.4f x %6.4f x %6.4f\n",dim_sub.dx, dim_sub.dy, dim_sub.dz);
+      printf("Target image file: %s\n",targetImageFile);
+      printf("\tMatrix size: %d x %d x %d\n",dim_trg.nx, dim_trg.ny, dim_trg.nz);
+      printf("\tVoxel size: %6.4f x %6.4f x %6.4f\n",dim_trg.dx, dim_trg.dy, dim_trg.dz);
+   }
+
+   ////////////////////////////////////////////////////////////////////////////////////////////
+   // find subTPIL and trgTPIL using automated MSP, AC/PC and 8 MSP landmarks detection
    if(opt_I==YES)
    {
       set_to_I(subTPIL,4);
+      set_to_I(trgTPIL,4);
    }
    else {
       // find subTPIL 
       if(opt_v) printf("Computing subject image PIL transformation ...\n");
       if(sublmfile[0] != '\0' && opt_v) printf("Subject image landmarks are read from %s\n",sublmfile);
       new_PIL_transform(subjectImageFile, sublmfile, subTPIL);
-   }
 
-   if(opt_I==YES)
-   {
-      set_to_I(trgTPIL,4);
-   }
-   else {
       // find trgTPIL
       if(opt_v) printf("Computing target image PIL transformation ...\n");
       if(trglmfile[0] != '\0' && opt_v) printf("Target image landmarks are read from %s\n",trglmfile);
       new_PIL_transform(targetImageFile, trglmfile, trgTPIL);
    }
-
+   ////////////////////////////////////////////////////////////////////////////////////////////
+   
+   // compute trgPIL1, trgPIL2, trgPIL4 and trgPIL8
    generateMultiResolution(trg, dim_trg, trgTPIL, &trgPIL1, &trgPIL2, &trgPIL4, &trgPIL8);
 
-   // generate the masks
+   ////////////////////////////////////////////////////////////////////////////////////////////
+   // compute mskPIL1, mskPIL2, mskPIL4 and mskPIL8
    if(opt_I == YES)
    {
       set_to_I(I,4);
@@ -766,13 +761,19 @@ int main(int argc, char **argv)
 
       free(PILbraincloud);
    }
+   ////////////////////////////////////////////////////////////////////////////////////////////
 
-   float A[16];
+   ////////////////////////////////////////////////////////////////////////////////////////////
+   // Compute the affine transformation  sub_to_trg and update subTPIL
 
    if(opt_v) printf("Patch radius = %d mm\n", patch_r);
-   if(opt_v) printf("search radius = %d mm\n", search_R);
+   if(opt_v) printf("Search radius = %d mm\n", search_R);
 
-   if( affineTransformationFile[0] != '\0' )
+   if(opt_I == YES)
+   {
+      set_to_I(sub_to_trg,4);
+   }
+   else if( affineTransformationFile[0] != '\0' ) // -I option trumps -T
    {
       if( loadTransformation(affineTransformationFile,sub_to_trg)== 0 ) // no error
          multi(trgTPIL,4,4,sub_to_trg,4,4,subTPIL);  // update subTPIL according to A
@@ -782,12 +783,12 @@ int main(int argc, char **argv)
          exit(0);
       }
    }
-   else if(opt_I == YES)
-   {
-      set_to_I(sub_to_trg,4);
-   }
    else
    {
+      float *invT;		
+      float A[16];
+      FILE *fp;
+
       if(opt_v) printf("Affine registration @ 12.5\% resolution ...\n");
       generateMultiResolution(sub, dim_sub, subTPIL, &subPIL1, &subPIL2, &subPIL4, &subPIL8);
       affineReg(subPIL8, trgPIL8, mskPIL8, dim8, patch_r, search_R, A);
@@ -815,10 +816,13 @@ int main(int argc, char **argv)
    }
 
    if(opt_v) printMatrix(sub_to_trg,4,4,"subject image -> target image affine transformation",NULL);
+   ////////////////////////////////////////////////////////////////////////////////////////////
 
    ////////////////////////////////////////////////////////////////////////////////////////////
+   // Save affine transformed subject image
    {
       short *tmp;
+      float *invT;		
 
       invT=inv4(sub_to_trg);
       tmp = resliceImage(sub, dim_sub, dim_trg, invT, LIN); 
@@ -837,17 +841,20 @@ int main(int argc, char **argv)
    }
    //////////////////////////////////////////////////////////////////
    
+   //////////////////////////////////////////////////////////////////
+   // Non-linear registration from trgPIL to subPIL
+   
+   // intermediate displacement field from trgPIL to subPIL
+   float *Xwarp=NULL, *Ywarp=NULL, *Zwarp=NULL;
+
    Xwarp = (float *)calloc(dim1.nv, sizeof(float));
    Ywarp = (float *)calloc(dim1.nv, sizeof(float));
    Zwarp = (float *)calloc(dim1.nv, sizeof(float));
 
-
    if(iter8>=1)
    {
       if(opt_v) printf("Non-linear registration @ 12.5\% resolution ...\n");
-      generateMultiResolution(sub, dim_sub, subTPIL, &subPIL1, &subPIL2, &subPIL4, &subPIL8);
-      nlReg(subPIL8, trgPIL8, mskPIL8, dim8, patch_r, search_R/3, Xwarp, Ywarp, Zwarp,5);
-      for(int i=1; i<iter8; i++)
+      for(int i=0; i<iter8; i++)
       {
          generateMultiResolution(sub, dim_sub, subTPIL, Xwarp, Ywarp, Zwarp, &subPIL1, &subPIL2, &subPIL4, &subPIL8);
          nlReg(subPIL8, trgPIL8, mskPIL8, dim8, patch_r, search_R/3, Xwarp, Ywarp, Zwarp,5);
@@ -883,35 +890,44 @@ int main(int argc, char **argv)
          nlReg(subPIL1, trgPIL1, mskPIL1, dim1, patch_r, search_R, Xwarp, Ywarp, Zwarp,5);
       }
    }
+   //////////////////////////////////////////////////////////////////
 
+   //////////////////////////////////////////////////////////////////
+   // combine trgTPIL, (Xwarp,Ywarp,Zwarp) and subTPIL to obtain (Xout,Yout,Zout)
+
+   // final displacement field from trg to sub
+   float *Xout=NULL, *Yout=NULL, *Zout=NULL; 
+
+   Xout = (float *)calloc(dim_trg.nv, sizeof(float));
+   Yout = (float *)calloc(dim_trg.nv, sizeof(float));
+   Zout = (float *)calloc(dim_trg.nv, sizeof(float));
+
+   combine_warps_and_trans(dim1.nx, dim1.ny, dim1.nz, dim1.dx, dim1.dy, dim1.dz, Xwarp, Ywarp, Zwarp, subTPIL);
+   combine_warps_and_trans(trgTPIL, Xwarp, Ywarp, Zwarp, Xout, Yout, Zout, dim_trg);
+
+   ////////////////////////////////////////////////////////////////////////////////////////////
+   // saved warped subject image
    {
       short *Csub;
-      float T[16];
-
-      Xout = (float *)calloc(dim_trg.nv, sizeof(float));
-      Yout = (float *)calloc(dim_trg.nv, sizeof(float));
-      Zout = (float *)calloc(dim_trg.nv, sizeof(float));
-
-      combine_warps_and_trans(dim1.nx, dim1.ny, dim1.nz, dim1.dx, dim1.dy, dim1.dz, Xwarp, Ywarp, Zwarp, subTPIL);
-
-      combine_warps_and_trans(trgTPIL, Xwarp, Ywarp, Zwarp, Xout, Yout, Zout, dim_trg);
 
       Csub=computeReslicedImage2(sub, dim_sub, dim_trg, Xout, Yout, Zout);
 
       if(outputfile[0]=='\0') sprintf(filename,"C%s.nii",subprefix);
       else sprintf(filename,"%s",outputfile);
-      if(opt_v) printf("Saving output (warped) image file %s ...\n",filename);
+      if(opt_v) printf("Saving non-linearly transformed subject image in %s ...\n",filename);
       save_nifti_image(filename, Csub, &trg_hdr);
 
       free(Csub);
    }
-
    ////////////////////////////////////////////////////////////////////////////////////////////
 
+   ////////////////////////////////////////////////////////////////////////////////////////////
+   // save (Xout, Yout, Zout)
    {
       float min, max, s=0.0;
       short *sdum;
       nifti1_extender extender;
+      FILE *fp;
 
       if(warpfile[0]=='\0') sprintf(warpfile,"%s_wrp.nii",subprefix);
 
@@ -966,11 +982,18 @@ int main(int argc, char **argv)
 
       free(sdum);
    }
+   ////////////////////////////////////////////////////////////////////////////////////////////
 
+   ////////////////////////////////////////////////////////////////////////////////////////////
+   // free allocated memory 
    free(Xout); free(Yout); free(Zout);
    free(Xwarp); free(Ywarp); free(Zwarp);
    free(sub);
    free(trg);
+   free(trgPIL1); free(trgPIL2); free(trgPIL4); free(trgPIL8);
+   free(subPIL1); free(subPIL2); free(subPIL4); free(subPIL8);
+   free(mskPIL1); free(mskPIL2); free(mskPIL4); free(mskPIL8);
+   ////////////////////////////////////////////////////////////////////////////////////////////
 
    if(opt_v) printf("THE END\n");
 }
