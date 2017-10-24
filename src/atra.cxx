b562142ea3bd442fa5c383e1eb18672e4bce85d4
Babak A. Ardekani, Ph.D <ardekani@nki.rfmh.org>
2017-10-24 17:29:03 -0400

    *LM.ppm renamed to *ORION.ppm in PILtransform.cpp Minor change to atra.cxx verbose output

--- src/atra.cxx
+++ src/atra.cxx
@@ -694,7 +694,7 @@ void atra(const char *imagelistfile, DIM output_dim, const char *outputOrientati
       if(opt_v) printf("Iteration %d ...\n", iteration+1);
       //  if(opt_v) printf("Number of seeds = %d\n", nseeds);
       if(opt_v) printf("Number of LOOC landmarks identified = %d\n",nlm);
-      if(opt_v) printf("%5.2f%% aligned\n",100.0*current_nlm_aligned/nlm);
+      //if(opt_v) printf("%5.2f%% aligned\n",100.0*current_nlm_aligned/nlm);
 
       if(nlm<6)
       {
