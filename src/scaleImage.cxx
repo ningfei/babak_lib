8a0909861796796ba5cc8082a0c31ef6c91b018f
Ardekani <ardekb01@ARDEKB01-MBP15T.local>
2018-04-10 11:32:07 -0400

    Cosmetic changes to scaleImage.cxx

--- src/scaleImage.cxx
+++ src/scaleImage.cxx
@@ -20,24 +20,18 @@ int opt;
 
 static struct option options[] =
 {
-		{"-version", 0, 'V'},
-		{"-V", 0, 'V'},
-
-        {"-verbose", 0, 'v'},
-        {"-v", 0, 'v'},
-
-        {"-float", 0, 'f'},
-        {"-f", 0, 'f'},
-
-        {"-short", 0, 's'},
-        {"-s", 0, 's'},
-
-        {"-prefix", 1, 'p'},
-        {"-p", 1, 'p'},
-
-        {"-scale", 1, 'c'},
-
-        {0, 0, 0}
+  {"-version", 0, 'V'},
+  {"-V", 0, 'V'},
+  {"-verbose", 0, 'v'},
+  {"-v", 0, 'v'},
+  {"-float", 0, 'f'},
+  {"-f", 0, 'f'},
+  {"-short", 0, 's'},
+  {"-s", 0, 's'},
+  {"-prefix", 1, 'p'},
+  {"-p", 1, 'p'},
+  {"-scale", 1, 'c'},
+  {0, 0, 0}
 };
 
 int opt_f=NO;
@@ -47,187 +41,176 @@ int opt_scale=NO;
 	
 void print_help_and_exit()
 {
-	printf("\nUsage: scaleImage [-V -v -f -s] [-scale <scale>] [-prefix <prefix>] <image files ...>\n\n"
-	"-V or -version : prints date of last revision\n\n"
-	"-v or -verbose : sets verbose mode\n\n"
-	"-f or -float : output image is saved as type 'float'\n\n"
-	"-s or -short : output image is saved as type 'short'\n\n"
-	"-p or -prefix : prefix for naming output images (default='scl_')\n\n"
-	"-scale : each voxel value is multiplied by this number (default=1.0)\n\n"
-	);
-	exit(0);
+  printf("\nUsage: scaleImage [-V -v -f -s] [-scale <scale>] [-prefix <prefix>] <image files ...>\n\n"
+  "-V or -version : prints date of last revision\n\n"
+  "-v or -verbose : sets verbose mode\n\n"
+  "-f or -float : output image is saved as type 'float'\n\n"
+  "-s or -short : output image is saved as type 'short'\n\n"
+  "-p or -prefix : prefix for naming output images (default='scl_')\n\n"
+  "-scale : each voxel value is multiplied by this number (default=1.0)\n\n"
+  );
+  exit(0);
 }
 
 int main(int argc, char **argv)
 {
-	nifti_1_header hdr;
-	char prefix[1024];
-	char tempfile[1024];
-	char opfile[1024];
-	char *im;
-	char **imagefile;
+  nifti_1_header hdr;
+  char prefix[1024];
+  char tempfile[1024];
+  char opfile[1024];
+  char *im;
+  char **imagefile;
+  float scale=1.0;
+  float dx,dy,dz;
+  float *fim;
+  int n;
+  int nx, ny, nz, datatype;
+  int nv;
+  short *sim;
+  unsigned char *ucim;
 
-	float scale=1.0;
-	float dx,dy,dz;
-	float *fim;
+  while( (opt=getoption(argc, argv, options)) != -1)
+  {
+    switch (opt) {
+      case 'V':
+        printf("\n\nLast modified: April 15, 2005\n\n");
+        exit(0);
+      case 'v':
+        opt_v=YES;
+        break;
+      case 's':
+        opt_s=YES;
+        break;
+      case 'f':
+        opt_f=YES;
+        break;
+      case 'p':
+        sprintf(prefix,"%s",optarg);
+        opt_p=YES;
+        break;
+      case 'c':
+        scale = atof(optarg);
+        opt_scale=YES;
+        break;
+      case '?':
+        print_help_and_exit();
+    }
+  }
 
-	int n;
-	int nx, ny, nz, datatype;
-	int nv;
+  if(argc==1) print_help_and_exit();
 
-	short *sim;
-    unsigned char *ucim;
+  if(opt_s && opt_f) 
+  {
+    printf("\n -s (-short) and -f (-float) options cannot be used together!\n");
+    exit(0);
+  }
 
-	while( (opt=getoption(argc, argv, options)) != -1)
-	{
-		switch (opt) {
-			case 'V':
-				printf("\n\nLast modified: April 15, 2005\n\n");
-				exit(0);
-			case 'v':
-				opt_v=YES;
-				break;
-			case 's':
-				opt_s=YES;
-				break;
-			case 'f':
-				opt_f=YES;
-				break;
-			case 'p':
-				sprintf(prefix,"%s",optarg);
-				opt_p=YES;
-				break;
-			case 'c':
-				scale = atof(optarg);
-				opt_scale=YES;
-				break;
-			case '?':
-				print_help_and_exit();
-		}
-	}
+  if(!opt_p) sprintf(prefix,"scl_");
 
-	if(argc==1) print_help_and_exit();
+  if(opt_v && opt_p) printf("\nOutput image prefix is '%s'\n",prefix);
 
-	if(opt_s && opt_f) 
-	{
-		printf("\n -s (-short) and -f (-float) options cannot be used together!\n");
-		exit(0);
-	}
+  if(opt_v) printf("\nScale = %f\n",scale);
 
-	if(!opt_p) sprintf(prefix,"scl_");
+  n = argc-optind;
+  if(opt_v) printf("\nNumber of input images = %d\n",n);
 
-	if(opt_v && opt_p) printf("\nOutput image prefix is '%s'\n",prefix);
+  if(n<=0) exit(0);
 
-	if(opt_v) printf("\nScale = %f\n",scale);
+  imagefile = argv+optind;
 
-    n = argc-optind;
-    if(opt_v) printf("\nNumber of input images = %d\n",n);
+  if(opt_v) 
+  {
+    printf("\nInput images:\n");
+    for(int i=0; i<n; i++)
+      printf("\tImage %d = %s\n",i+1,imagefile[i]);
+  }
 
-	if(n<=0) exit(0);
+  for(int i=0; i<n; i++)
+  {
+    im = NULL;
+    im = read_nifti_image(imagefile[i], &hdr);
+    nx = hdr.dim[1]; ny = hdr.dim[2]; nz = hdr.dim[3];
+    dx = hdr.pixdim[1]; dy = hdr.pixdim[2]; dz = hdr.pixdim[3];
+    datatype = hdr.datatype;
 
-    imagefile = argv+optind;
+    nv = nx*ny*nz;
 
-    if(opt_v) 
-	{
-		printf("\nInput images:\n");
-		for(int i=0; i<n; i++)
-			printf("\tImage %d = %s\n",i+1,imagefile[i]);
-	}
+    // I added this because some SPM ANALYZE images have negative dimensions
+    if(dx<0) dx *= -1; if(dy<0) dy *= -1; if(dz<0) dz *= -1;
 
-	for(int i=0; i<n; i++)
-	{
-		im = NULL;
-		im = read_nifti_image(imagefile[i], &hdr);
-		nx = hdr.dim[1]; ny = hdr.dim[2]; nz = hdr.dim[3];
-		dx = hdr.pixdim[1]; dy = hdr.pixdim[2]; dz = hdr.pixdim[3];
-		datatype = hdr.datatype;
+    if(im==NULL) continue;
 
-		nv = nx*ny*nz;
+    // if(datatype!=4 && datatype!=16)
+    if(datatype!=2 && datatype!=4 && datatype!=16)
+    {
+      printf("\nCannot process datatype %d, skipping this image ...\n",datatype);
+      continue;
+    }
 
-		// I added this because some SPM ANALYZE images have negative dimensions
-		if(dx<0) dx *= -1; if(dy<0) dy *= -1; if(dz<0) dz *= -1;
+    if(opt_v)
+    {
+      printf("\nProcessing image %d\n", i+1);
+      printf("\tImage filename = %s\n",imagefile[i]);
+      if( datatype == 2 ) printf("\tInput image data type = 'unsigned char'\n");
+      if( datatype == 4 ) printf("\tInput image data type = 'short'\n");
+      if( datatype == 16 ) printf("\tInput image data type = 'float'\n");
+      printf("\tMatrix size = %d x %d x %d\n",nx,ny,nz);
+      printf("\tVoxel size = %f x %f x %f\n",dx,dy,dz);
+    }
 
-		if(im==NULL) continue;
+    getfilename(tempfile,imagefile[i]);
+    sprintf(opfile,"%s%s",prefix,tempfile);
 
-		// if(datatype!=4 && datatype!=16)
-		if(datatype!=2 && datatype!=4 && datatype!=16)
-		{
-			printf("\nCannot process datatype %d, skipping this image ...\n",datatype);
-			continue;
-		}
+    if(opt_v) printf("\tOutput image filename = %s\n",opfile);
 
-		if(opt_v)
-		{
-			printf("\nProcessing image %d\n", i+1);
-			printf("\tImage filename = %s\n",imagefile[i]);
-			if( datatype == 2 ) printf("\tInput image data type = 'unsigned char'\n");
-			if( datatype == 4 ) printf("\tInput image data type = 'short'\n");
-			if( datatype == 16 ) printf("\tInput image data type = 'float'\n");
-			printf("\tMatrix size = %d x %d x %d\n",nx,ny,nz);
-			printf("\tVoxel size = %f x %f x %f\n",dx,dy,dz);
-		}
+    if( datatype==2 )
+    {
+      ucim = (unsigned char *)im;
+      for(int v=0; v<nv; v++)  ucim[v] = (unsigned char)( ucim[v]*scale );
+      if(opt_v) printf("\tOutput image data type = 'unsigned char'\n");
+      save_nifti_image(opfile, ucim, &hdr);
+    }
 
-		getfilename(tempfile,imagefile[i]);
-		sprintf(opfile,"%s%s",prefix,tempfile);
+    if( datatype==4 )
+    {
+      sim = (short *)im;
 
-		if(opt_v) printf("\tOutput image filename = %s\n",opfile);
+      if(opt_f)
+      {
+        fim = (float *)calloc(nv,sizeof(float));
+        for(int v=0; v<nv; v++)  fim[v] = sim[v]*scale;
+        if(opt_v) printf("\tOutput image data type = 'float'\n");
+        save_nifti_image(opfile, fim, &hdr);
+        free(fim);
+      }
+      else 
+      {
+        for(int v=0; v<nv; v++)  sim[v] = (short)( sim[v]*scale + 0.5);
+        if(opt_v) printf("\tOutput image data type = 'short'\n");
+        save_nifti_image(opfile, sim, &hdr);
+      }
+    }
 
-		if( datatype==2 )
-		{
-			ucim = (unsigned char *)im;
+    if( datatype==16 )
+    {
+      fim = (float *)im;
 
-			for(int v=0; v<nv; v++)  ucim[v] = (unsigned char)( ucim[v]*scale );
+      if(opt_s)
+      {
+        sim = (short *)calloc(nv,sizeof(short));
+        for(int v=0; v<nv; v++)  sim[v] = (short) (fim[v]*scale + 0.5);
+        if(opt_v) printf("\tOutput image data type = 'short'\n");
+        save_nifti_image(opfile, sim, &hdr);
+        free(sim);
+      }
+      else
+      {
+        for(int v=0; v<nv; v++)  fim[v] *= scale;
+        if(opt_v) printf("\tOutput image data type = 'float'\n");
+        save_nifti_image(opfile, fim, &hdr);
+      }
+    }
 
-            if(opt_v) printf("\tOutput image data type = 'unsigned char'\n");
-			save_nifti_image(opfile, ucim, &hdr);
-		}
-
-		if( datatype==4 )
-		{
-			sim = (short *)im;
-
-			if(opt_f)
-			{
-				fim = (float *)calloc(nv,sizeof(float));
-				for(int v=0; v<nv; v++)  fim[v] = sim[v]*scale;
-
-				if(opt_v) printf("\tOutput image data type = 'float'\n");
-				save_nifti_image(opfile, fim, &hdr);
-
-
-				free(fim);
-			}
-		 	else 
-			{
-				for(int v=0; v<nv; v++)  sim[v] = (short)( sim[v]*scale + 0.5);
-
-				if(opt_v) printf("\tOutput image data type = 'short'\n");
-				save_nifti_image(opfile, sim, &hdr);
-			}
-		}
-
-
-		if( datatype==16 )
-		{
-			fim = (float *)im;
-
-			if(opt_s)
-			{
-				sim = (short *)calloc(nv,sizeof(short));
-				for(int v=0; v<nv; v++)  sim[v] = (short) (fim[v]*scale + 0.5);
-
-				if(opt_v) printf("\tOutput image data type = 'short'\n");
-				save_nifti_image(opfile, sim, &hdr);
-				free(sim);
-			}
-			else
-			{
-				for(int v=0; v<nv; v++)  fim[v] *= scale;
-				if(opt_v) printf("\tOutput image data type = 'float'\n");
-				save_nifti_image(opfile, fim, &hdr);
-			}
-		}
-
-		free(im);
-	}
+    free(im);
+  }
 }
